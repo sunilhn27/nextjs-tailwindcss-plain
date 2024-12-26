a5b7f84c6bbc766f92c6062769508405608d7cbe
Sunil H N <sunilh.n27@gmail.com>
2024-12-26 19:54:54 +0530

    added nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "./../../public/asserts/Link_margin.png";
+
+function NavBar() {
+  return (
+    <section className="py-4 sticky top-0 bg-white shadow-md z-50">
+      <div className="">
+        <nav className="flex mx-[3.5rem] justify-around text-center ">
+          <Link href={"/"}>
+            <Image src={Logo} alt="Logo"></Image>
+          </Link>
+          <div className="flex space-x-6 justify-center items-center text-xl text-gray-600 ">
+            <Link className="hover:text-orange-500 no-underline" href={"/"}>
+              About
+            </Link>
+            <Link
+              className="hover:text-orange-500 cursor-pointer no-underline"
+              href={"/"}
+            >
+              Services
+            </Link>
+            <Link
+              className="hover:text-orange-500 cursor-pointer no-underline"
+              href={"/"}
+            >
+              Home
+            </Link>
+            <Link
+              className="hover:text-orange-500 cursor-pointer no-underline"
+              href={"/"}
+            >
+              Projects
+            </Link>
+            <Link
+              className="hover:text-orange-500 cursor-pointer no-underline"
+              href={"/"}
+            >
+              Blog
+            </Link>
+            <Link
+              className="hover:text-orange-500 cursor-pointer no-underline"
+              href={"/"}
+            >
+              Contact
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+}
+
+export default NavBar;
