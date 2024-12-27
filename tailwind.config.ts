71ca66ab26c6a813e5a4df72dc2c51abfa0e3842
Sunil H N <sunilh.n27@gmail.com>
2024-12-27 09:02:04 +0530

    added font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,13 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    
   ],
   theme: {
     extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"], // Add Nunito font
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
