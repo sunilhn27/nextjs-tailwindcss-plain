9ef6445858a80c24fcdd699c42449792522ade9d
Sunil H N <sunilh.n27@gmail.com>
2024-12-31 09:27:22 +0530

    added animation to services

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -2,6 +2,7 @@ import Image, { StaticImageData } from "next/image";
 import React from "react";
 import CreativeDesign from "../../public/images/creative-design.png";
 import webdesign from "../../public/images/webdesign.png";
+import { AnimationTop } from "./AnimationWrapper";
 
 interface Work {
   icon: StaticImageData;
@@ -33,16 +34,35 @@ function Works() {
       <div className="flex justify-center items-center text-center">
         <div className="flex flex-col">
           <h3 className="text-[#F94F4F]">Works</h3>
-          <h1 className="mt-0 w-[21rem] font-bold text-3xl">Some of our Recent Works</h1>
+          <h1 className="mt-0 w-[21rem] font-bold text-3xl">
+            Some of our Recent Works
+          </h1>
         </div>
       </div>
 
       <div className="flex justify-center items-center mt-10">
         <div className="grid grid-cols-4 2xl:grid-cols-5 gap-6">
-          <Work icon={CreativeDesign} title="Creative Design" param="UIdeck" />
-          <Work icon={webdesign} title="Web Design" param="UIdeck" />
-          <Work icon={CreativeDesign} title="Creative Design" param="UIdeck" />
-          <Work icon={webdesign} title="Analysis" param="UIdeck" />
+          <AnimationTop delay={1}>
+            <Work
+              icon={CreativeDesign}
+              title="Creative Design"
+              param="UIdeck"
+            />
+          </AnimationTop>
+          <AnimationTop delay={1}>
+            <Work icon={webdesign} title="Web Design" param="UIdeck" />
+          </AnimationTop>
+
+          <AnimationTop delay={1}>
+            <Work
+              icon={CreativeDesign}
+              title="Creative Design"
+              param="UIdeck"
+            />
+          </AnimationTop>
+          <AnimationTop delay={1}>
+            <Work icon={webdesign} title="Analysis" param="UIdeck" />{" "}
+          </AnimationTop>
           {/* Conditionally show these items only for 2xl screens */}
           <div className="hidden 2xl:block">
             <Work icon={webdesign} title="SEO" param="UIdeck" />
