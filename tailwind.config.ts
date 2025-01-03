0f27861c1fc8613fd9100f6e67a7a218db046b80
Sunil H N <sunilh.n27@gmail.com>
2025-01-03 09:12:00 +0530

    added tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,13 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    
+
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'custom-image': "url('/images/bg-image.png')",
+      },
       fontFamily: {
         nunito: ["Nunito", "sans-serif"], // Add Nunito font
       },
